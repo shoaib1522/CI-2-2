fc0995a0f52838346e5a772a475bcfabaff3f1ab
SHOAIB MUGHAL <shoaib1522>
2025-07-20 20:45:32 +0500

    Video 2 - 1

--- slugify.js
+++ slugify.js
@@ -8,7 +8,7 @@ function slugify(text) {
     .toString()
     .toLowerCase()
     .trim()
-    // .replace(/\s+/g, '-')      // Replace spaces with -
+    .replace(/\s+/g, '-')      // Replace spaces with -
     .replace(/[^\w\-]+/g, '')  // Remove all non-word chars
     .replace(/\-\-+/g, '-');   // Replace multiple - with single -
 }
