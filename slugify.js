9af85395fc1176651f3758dccc034e6dfc0833e7
SHOAIB MUGHAL <shoaib1522>
2025-07-20 20:10:16 +0500

    CI-pipeline test -02

--- slugify.js
+++ slugify.js
@@ -7,7 +7,7 @@ function slugify(text) {
   return text
     .toString()
     .toLowerCase()
-    .trim()
+    // .trim()
     .replace(/\s+/g, '-')      // Replace spaces with -
     .replace(/[^\w\-]+/g, '')  // Remove all non-word chars
     .replace(/\-\-+/g, '-');   // Replace multiple - with single -
