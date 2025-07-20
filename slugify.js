ccca85450ceb5f91825a34e8bfde6bdc1c105242
SHOAIB MUGHAL <shoaib1522>
2025-07-20 20:11:57 +0500

    CI-pipeline test -02

--- slugify.js
+++ slugify.js
@@ -7,8 +7,8 @@ function slugify(text) {
   return text
     .toString()
     .toLowerCase()
-    // .trim()
-    .replace(/\s+/g, '-')      // Replace spaces with -
+    .trim()
+    // .replace(/\s+/g, '-')      // Replace spaces with -
     .replace(/[^\w\-]+/g, '')  // Remove all non-word chars
     .replace(/\-\-+/g, '-');   // Replace multiple - with single -
 }
